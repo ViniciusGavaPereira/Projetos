39f6d450c07ec239693e3e86a15643aadd33bc5e
ViniciusGavaPereira <vini-gava@outlook.com.br>
2023-04-29 19:59:11 -0300

    Classe singleton criada

--- Padroes de Projetos/Criacional/Singleton/src/App.java
+++ Padroes de Projetos/Criacional/Singleton/src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, Singleton World!");
+
+        Singleton pessoa1 = Singleton.getInstance();
+        Singleton pessoa2 = Singleton.getInstance();
+
+        //Pode perceber que o hashcode das duas pessoas é o mesmo
+        System.out.println(pessoa1);
+        System.out.println(pessoa2);
+
     }
 }
